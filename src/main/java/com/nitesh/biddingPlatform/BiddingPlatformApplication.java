4b90dfc570a2ca5f2ae6c7f1036e32ca098790c5
Nitesh Jain <nitesh.jain@iiitb.org>
2021-04-02 21:26:37 +0530

    Project started

---
+++ src/main/java/com/nitesh/biddingPlatform/BiddingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.nitesh.biddingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitesh/biddingPlatform/BiddingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitesh.biddingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
